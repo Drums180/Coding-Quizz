a6da0d4531ca857d57b2489773db933ac61bb780
Drums180 <daviddomor180@hotmail.com>
2023-01-04 20:27:59 -0600

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
+// DEFINING VARIABLES OF ELEMENTS
+var home = document.querySelector(".start")
+var quizz = document.querySelector(".quizz")
+
 // TIMER
 var timeEl = document.querySelector(".time");
+var startButton = document.getElementById("goButton");
 
 var secondsLeft = 50;
 
-var countDown = setInterval(function() {
-    
-})
+function setTime() {
+    var countDown = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+
+     if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+}
+
+function lol() {
+    home.style.display = "none";
+    quizz.style.display = "y";
+}
+
